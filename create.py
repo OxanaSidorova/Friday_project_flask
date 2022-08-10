1834d48e053cd29210681a824315bf39c7169503
OxanaSidorova <106552165+OxanaSidorova@users.noreply.github.com>
2022-08-10 12:50:56 +0100

    initial commit

---
+++ create.py
@@ -0,0 +1,35 @@
+# Perform static database operations
+#
+from schema import *
+
+# Task -1 (only once)
+# d.create_all()
+
+# Task - 2 (only once)
+c1 = Colleges(college_id=1,college_name='Oxford',address='UK',director_name='Oxana')
+d.session.add(c1)
+c2 = Colleges(college_id=2,college_name='Stanford',address='US',director_name='Paresh')
+d.session.add(c2)
+c3 = Colleges(college_id=3,college_name='Harvard',address='US',director_name='Alexia')
+d.session.add(c3)
+c4 = Colleges(college_id=4,college_name='Cambridge',address='UK',director_name='Rajwant')
+d.session.add(c4)
+
+d.session.commit()
+
+p1 = Programs(program_id=1,program_name='M.S. Data Science',fees=5000,college_id=1)
+d.session.add(p1)
+p2 = Programs(program_id=2,program_name='M.S. IoT',fees=7000,college_id=1)
+d.session.add(p2)
+p3 = Programs(program_id=3,program_name='M.S. Machine Learning',fees=10000,college_id=4)
+d.session.add(p3)
+p5 = Programs(program_id=40,program_name='M.S. Cybersecurity',fees=6000,college_id=1)
+d.session.add(p5)
+
+d.session.commit()
+
+# Task - 3 (multiple times)
+# for clg in Colleges.query.all():
+#     print("- ",clg.college_name)
+#     for program in clg.programs:
+#         print("\t - ",program.program_name)
